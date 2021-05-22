621b77798b463e6a66b85fc60f5e946998376b49
NguyenBaoChau2604 <71972910+NguyenBaoChau2604@users.noreply.github.com>
2021-05-22 17:13:05 +0700

    Add files via upload

---
+++ yolo_image.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+
+# tai yolo
+net = cv2.dnn.readNet("weights/yolov3.weights", "cfg/yolov3.cfg")
+classes = []
+with open("coco.names", "r") as f:
+    classes = [line.strip() for line in f.readlines()]
+layer_names = net.getLayerNames()
+output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+colors = np.random.uniform(0, 255, size=(len(classes), 3))
+
+# tai anh
+img = cv2.imread("input/images/cam3.jpeg")
+'''img = cv2.resize(img, None, fx=0.5, fy=0.5)'''
+height, width, channels = img.shape
+
+# nhan dien cac doi tuong
+blob = cv2.dnn.blobFromImage(img, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
+
+net.setInput(blob)
+outs = net.forward(output_layers)
+
+# hien thong tin nhan cua cac lop
+class_ids = []
+confidences = []
+boxes = []
+for out in outs:
+    for detection in out:
+        scores = detection[5:]
+        class_id = np.argmax(scores)
+        confidence = scores[class_id]
+        if confidence > 0.5:
+            # nhan dang tung doi tuong
+            center_x = int(detection[0] * width)
+            center_y = int(detection[1] * height)
+            w = int(detection[2] * width)
+            h = int(detection[3] * height)
+
+            # ve khung chu nhat (bounding box)
+            x = int(center_x - w / 2)
+            y = int(center_y - h / 2)
+
+            boxes.append([x, y, w, h])
+            confidences.append(float(confidence))
+            class_ids.append(class_id)
+
+indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
+print(indexes)
+font = cv2.FONT_HERSHEY_PLAIN
+for i in range(len(boxes)):
+    if i in indexes:
+        x, y, w, h = boxes[i]
+        label = str(classes[class_ids[i]])
+        color = colors[class_ids[i]]
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        cv2.putText(img, label, (x, y-5), 0, 0.75, color, 2)
+
+
+cv2.imshow("Image", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
